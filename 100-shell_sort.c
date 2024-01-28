b93c6cdbbca03685732d4c0459b633bb1bb44465
StephenAnyanwu <voltasteve@gmailcom>
2024-01-28 06:05:54 -0800

    task4 corr1

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,16 +8,21 @@
  *
  * Description: impliment using Knuth sequence,
  * 1, 4, 13, 40, 121, 364,...
+ * Print array each time the interval of Knuth sequence
+ * is decreased
  */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap, h, j, k;
 	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
 	h = 1;
 	/* compute last value of Knuth sequence */
 	while (h < size)
 		h = 3 * h + 1;
+	/* h is greater than size, hence gap starts from h/3 */
 	for (gap = h / 3; gap > 0; gap /= 3)
 	{
 		for (j = gap; j < size; j++)
@@ -35,3 +40,4 @@ void shell_sort(int *array, size_t size)
 		print_array(array, size);
 	}
 }
+
