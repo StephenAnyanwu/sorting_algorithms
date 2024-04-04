eb13799eb41c26233db4e821f300107d5dd2c26f
StephenAnyanwu <voltasteve@gmailcom>
2024-04-04 07:13:16 -0700

    sort.h correction

--- sort.h
+++ sort.h
@@ -13,15 +13,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
-
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
